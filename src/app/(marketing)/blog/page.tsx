3fc1d9e6c751ada3a861701368c272cc44770a04
Kens04 <kento090403@icloud.com>
2024-05-26 17:43:17 +0900

    ブログページのレイアウトの修正

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,5 +1,7 @@
 import { allPosts } from "contentlayer/generated";
+import { format } from "date-fns";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function BlogPage() {
   const posts = allPosts;
@@ -17,8 +19,9 @@ export default function BlogPage() {
         </div>
       </div>
       <hr className="my-8" />
+      <div className="grid sm:grid-cols-2 gap-10">
       {posts.map((post) => (
-        <article key={post._id}>
+        <article key={post._id} className="relative flex flex-col space-y-2">
           {post.image && (
             <Image
               src={post.image}
@@ -33,10 +36,14 @@ export default function BlogPage() {
             <p className="text-muted-foreground">{post.description}</p>
           )}
           {post.date && (
-            <p className="text-sm text-muted-foreground">{post.date}</p>
+            <p className="text-sm text-muted-foreground">
+              {format(post.date, "yyyy/MM/dd")}
+            </p>
           )}
+          <Link href={post.slug} className="absolute inset-0"></Link>
         </article>
       ))}
+      </div>
     </div>
   );
 }
