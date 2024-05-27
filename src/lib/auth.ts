e7f256d350bb67187204aa7dc371ed0a0d1f79ce
Kens04 <kento090403@icloud.com>
2024-05-27 23:15:34 +0900

    GithubでOauth認証のセットアップ

---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import Github from "next-auth/providers/github";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+};
